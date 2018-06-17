19a3d491c5d215a6de36310e8a3a04b87a0f1feb
Bennett Cheung <bennettcheung@yahoo.com>
2018-06-16 18:51:56 -0700

    Initial Commit

---
+++ Two Doors/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Two Doors
+//
+//  Created by Bennett on 2018-06-16.
+//  Copyright © 2018 Bennett. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
